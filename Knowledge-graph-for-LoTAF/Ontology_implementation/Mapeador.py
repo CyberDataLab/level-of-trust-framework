c6e5ee59001eae415619399ee2ef6e4317e7f036
AlfonsoTGR <alfonsoserra2003@gmail.com>
2024-12-15 20:01:55 +0100

    Mapping file done

--- Knowledge-graph-for-LoTAF/Ontology_implementation/Mapeador.py
+++ Knowledge-graph-for-LoTAF/Ontology_implementation/Mapeador.py
@@ -3,11 +3,11 @@ import morph_kgc
 # Definir la ruta de tu archivo de mapeo
 mapping_path =  """
                     [DataSource]
-                    mappings: C:\\Users\\Usuario\\Desktop\\Trabajo\\Pruebas_Ontologia\\lotaf-mapping.rml.ttl
+                    mappings: nuevo-mapping.rml.ttl
                 """
 
 # Generar las tripletas RDF
 g = morph_kgc.materialize(mapping_path)
 
 # Guardar las tripletas en un archivo RDF
-g.serialize(destination="C:\\Users\\Usuario\\Desktop\\Trabajo\\Pruebas_Ontologia\\output.rdf", format="xml")
+g.serialize(destination="output.rdf", format="xml")
